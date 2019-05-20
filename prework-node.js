fb714bcb62566d074ee88da13a4c349df7225746
ruhlen <rruhlen@salesforce.com>
2019-05-19 23:59:02 -0700

    prework for Node JS

---
+++ prework-node.js
@@ -0,0 +1,12 @@
+let loopyThing = function(param) {
+if (param > 10) {
+    console.log("Hey! That's a large number!")
+}
+else {
+    console.log("Hey! That's a small number!")
+}
+}
+
+loopyThing(12)
+console.log("===========================")
+loopyThing(5)
